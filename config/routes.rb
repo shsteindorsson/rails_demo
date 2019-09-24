2d1f86a4288df40c3390e362eaf26122dfdfa1fa
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-24 23:32:14 +0200

    Add steps to landing_page_steps, add path to route
    Add link to index.html.erb
    Add 'New Article' form to new.html.erb
    Add Articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'landing/index'
   root controller: :landing, action: :index
+  get 'articles/new' => "articles#new", as: :articles_new
 end
--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -11,3 +11,23 @@ Given(/^the following articles exists$/) do |table|
     Article.create!(hash)
   end
 end
+
+Given("I visit the {string} page") do |string|
+  visit root_path
+end
+
+When("I click {string} link") do |string|
+  click_link(string)
+end
+
+When("I fill in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
