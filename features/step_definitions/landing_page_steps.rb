b2f68713073201d4e935056c690484bfecb1e5f4
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-26 22:09:17 +0200

    Add 'show article' page
    Add method 'show' to articles_controller.
    Add step definition to landing_page_steps.

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -29,5 +29,5 @@ When("I click {string} button") do |string|
 end
 
 Then("I should be on {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content string
 end
