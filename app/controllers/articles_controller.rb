b019d6f57ea9357c5666e7553ad48b9b327a4d1c
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-27 14:48:38 +0200

    Add checks for empty fields when creating articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,25 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
   
   def new
+    @article = Article.new
   end
 
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
  
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
