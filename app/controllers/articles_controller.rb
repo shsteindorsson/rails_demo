1a58cc7c6c6acf06794455468a2928ae0ef32a53
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-28 11:09:35 +0200

    Add if-else statement to create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,12 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def create
